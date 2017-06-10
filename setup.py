c5d2d87b5068a6e6eff3ca0d0381f8f7fa2f1f3b
David Alexander <opensource@thelonelyghost.com>
2017-06-10 18:09:37 -0400

    Pip install as an option

---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import os
+import codecs
+from setuptools import (
+    setup,
+    find_packages,
+)
+from tor.__meta__ import __version__
+
+
+def long_description():
+    if not (os.path.isfile('README.rst') and os.access('README.rst', os.R_OK)):
+        return ''
+
+    with codecs.open('README.rst', encoding='utf8') as f:
+        return f.read()
+
+
+setup(
+    name='tor',
+    version=__version__,
+    description='',
+    long_description=long_description(),
+    url='https://github.com/itsthejoker/transcribersofreddit',
+    author='Joe Kaufeld',
+    author_email='joe.kaufeld@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Communications :: BBS',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='',
+    packages=find_packages(exclude=['test*', 'bin/*']),
+    install_requires=[
+        'praw==4.4.0',
+        'redis<3.0.0',
+        'hiredis',
+        'better_exceptions',
+        'addict',
+        'cython',
+        'tesserocr',
+        'imgurpython',
+        'wget',
+        'sh',
+        'bugsnag',
+    ]
+)
