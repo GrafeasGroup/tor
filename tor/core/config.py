e6c2d3dc52edf174361a6f55fe1cc32fe99122f6
David Alexander <opensource@thelonelyghost.com>
2017-06-10 20:21:40 -0400

    Adds initial test coverage for global config

--- tor/core/config.py
+++ tor/core/config.py
@@ -1,9 +1,89 @@
-import bugsnag
-from addict import Dict
+# Load configuration regardless of if bugsnag is setup correctly
+try:
+    import bugsnag
+except ImportError:
+    bugsnag = None
+
 from tor.__meta__ import __version__
 
-config = Dict()
 
+class BaseConfig:
+    '''
+    '''
+
+    # Whitelisted domains
+    domains = []
+
+    formatting = ''
+
+
+class VideoConfig(BaseConfig):
+    '''
+    '''
+
+
+class AudioConfig(BaseConfig):
+    '''
+    '''
+
+
+class ImageConfig(BaseConfig):
+    '''
+    '''
+
+
+class Subreddit:
+    '''
+    '''
+
+    def __init__(self):
+        '''
+        '''
+        # TODO: set if upvote filter is needed per-subreddit
+
+    def needs_upvote_filter(self):
+        '''
+        '''
+
+
+class Config:
+    '''
+    '''
+    media = {
+        'audio': AudioConfig,
+        'video': VideoConfig,
+        'image': ImageConfig,
+    }
+    mods = []
+    subreddits = []
+
+    bugsnag_api_key = None
+    slack_api_url = None
+    header = ''
+
+    no_gifs = []
+
+    perform_header_check = True
+    debug_mode = False
+
+    # Global flag to enable/disable placing the triggers
+    # for the OCR bot
+    OCR = True
+
+
+try:
+    Config.bugsnag_api_key = open('bugsnag.key').readline().strip()
+except FileNotFoundError:
+    Config.bugsnag_api_key = None
+
+try:
+    Config.slack_api_url = open('slack.key').readline().strip()
+except FileNotFoundError:
+    Config.slack_api_url = None
+
+
+# ----- Compatibility -----
+config = Config
 config.video_domains = []
 config.audio_domains = []
 config.image_domains = []
@@ -35,7 +115,7 @@ try:
 except FileNotFoundError:
     config.bs_api_key = None
 
-if config.bs_api_key:
+if bugsnag and config.bs_api_key:
     bugsnag.configure(
         api_key=config.bs_api_key,
         app_version=__version__
