3112a73e54ab4084f6b8b3a0a18530670a4bf426
Joe Kaufeld <joe.kaufeld@gmail.com>
2017-08-20 23:41:18 -0400

    moved flair definitions to tor_core

--- tor/strings/ocr.py
+++
@@ -1,8 +0,0 @@
-base_comment = (
-    'It looks like there\'s text in this image. I\'ve tried to transcribe '
-    'it automatically, but I\'m still learning -- this may be inaccurate. '
-    'At the very least, hopefully it will serve as a decent starting point '
-    'for your work!\n\nPlease note that any formatting instructions above '
-    'override whatever I provide, so please format my content accordingly '
-    'if you choose to use it.'
-)
