8532378b13491d2440d6ea4ed1150e98fb153e50
Joe Kaufeld <joe.kaufeld@gmail.com>
2017-11-14 20:39:44 -0500

    updating footer with new org URL

--- tor/strings/responses.py
+++ tor/strings/responses.py
@@ -2,7 +2,7 @@ bot_footer = (
     "{}\n\n---\n\n"
     "v{version} | This message was posted by a bot. "
     "| [FAQ](https://www.reddit.com/r/TranscribersOfReddit/wiki/index) "
-    "| [Source](https://github.com/itsthejoker/TranscribersOfReddit) "
+    "| [Source](https://github.com/GrafeasGroup/tor) "
     "| Questions? [Message the mods!](https://www.reddit.com/message/"
     "compose?to=%2Fr%2FTranscribersOfReddit&subject=Bot%20Question"
     "&message=)"
