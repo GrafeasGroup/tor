d6120b69b6fb14a9d596066c4c092e520dfdf5bb
Joe Kaufeld <joe.kaufeld@gmail.com>
2018-02-26 12:51:40 -0500

    update verbiage and include checklist

--- tor/strings/posts.py
+++ tor/strings/posts.py
@@ -3,17 +3,22 @@ discovered_submit_title = (
 )
 
 rules_comment = (
-    'If you would like to claim this post, please respond to this comment '
+    'If you would like to transcribe this post, please respond to this comment '
     'with the word `claiming` or `claim` in your response. I will '
     'automatically update the flair so that only one person is working on a '
     'post at any given time.'
     '\n\nWhen you\'re done, please comment again with `done`. Your flair will '
     'be updated to reflect the number of posts you\'ve transcribed and the '
     'will be marked as completed.'
+    '\n\nHere\'s [a checklist](https://www.reddit.com/r/TranscribersOfReddit'
+    'wiki/checklist) that might help you out -- not just for newbies!'
     '\n\nThis is a(n) {post_type} post, so please use the following formatting:'
     '\n\n---\n\n{formatting}\n\n---'
     '\n\nFooter\n---\n\nWhen you\'re done, please put the following footer at '
     'the **bottom** of your post:\n\n---\n\n{header}\n\n---'
+    '\n\nPlease report this post if it contains personal information or otherwise '
+    'breaks the rules of the subreddit we\'re linking to. The mods will check '
+    'and remove it shortly.'
     '\n\nIf you have any questions, feel free to [message the mods!]'
     '(https://www.reddit.com/message/'
     'compose?to=%2Fr%2FTranscribersOfReddit&subject=General%20Question'
