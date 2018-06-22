dd3198516c94eff526ee09dcbe0ad09d49f18993
Joe Kaufeld <joe.kaufeld@gmail.com>
2018-06-21 23:15:20 -0400

    rebuilt checking for new posts with threads

--- tor/core/posts.py
+++ tor/core/posts.py
@@ -17,30 +17,6 @@ from tor.strings.posts import rules_comment
 from tor.strings.posts import yt_already_has_transcripts
 
 
-def check_submissions(subreddit, config):
-    """
-    Loops through all of the subreddits that have opted in and pulls
-    the 10 newest submissions. It checks the domain of the submission
-    against the domain lists and hands off the post to process_post()
-    for formatting and posting on ToR.
-
-    :param subreddit: String. A valid subreddit name.
-    :param config: the config object.
-    :return: None.
-    """
-
-    sr = config.r.subreddit(subreddit).new(limit=10)
-
-    for post in sr:
-        if (
-            post.domain in config.image_domains or
-            post.domain in config.audio_domains or
-            post.domain in config.video_domains or
-            subreddit in config.subreddits_domain_filter_bypass
-        ):
-            process_post(post, config)
-
-
 def process_post(new_post, config):
     """
     After a valid post has been discovered, this handles the formatting
@@ -51,51 +27,50 @@ def process_post(new_post, config):
     :return: None.
     """
 
-    subreddit = new_post.subreddit
-
-    if subreddit.display_name in config.upvote_filter_subs:
+    if new_post['subreddit'] in config.upvote_filter_subs:
         # ignore posts if they don't meet the threshold for karma and the sub
         # is in our list of upvoted filtered ones
-        if new_post.ups < config.upvote_filter_subs[subreddit.display_name]:
+        if new_post['ups'] < config.upvote_filter_subs[new_post['subreddit']]:
             return
 
-    if not is_valid(new_post.fullname, config):
-        logging.debug(id_already_handled_in_db.format(new_post.fullname))
+    if not is_valid(new_post['name'], config):
+        logging.debug(id_already_handled_in_db.format(new_post['name']))
         return
 
-    if new_post.archived:
+    if new_post['archived']:
         return
 
-    if new_post.author is None:
+    if new_post['author'] is None:
         # we don't want to handle deleted posts, that's just silly
         return
 
     logging.info(
-        f'Posting call for transcription on ID {new_post.fullname} posted by '
-        f'{new_post.author.name} '
+        f'Posting call for transcription on ID {new_post["name"]} posted by '
+        f'{new_post["author"]}'
     )
 
-    if new_post.domain in config.image_domains:
+    if new_post['domain'] in config.image_domains:
         content_type = 'image'
         content_format = config.image_formatting
 
-    elif new_post.domain in config.audio_domains:
+    elif new_post['domain'] in config.audio_domains:
         content_type = 'audio'
         content_format = config.audio_formatting
 
-    elif new_post.domain in config.video_domains:
-        if 'youtu' in new_post.domain:
-            if not valid_youtube_video(new_post.url):
-                add_complete_post_id(new_post.fullname, config)
+    elif new_post['domain'] in config.video_domains:
+        if 'youtu' in new_post['domain']:
+            if not valid_youtube_video(new_post['url']):
+                add_complete_post_id(new_post['name'], config)
                 return
-            if get_yt_transcript(new_post.url):
-                new_post.reply(_(
+            if get_yt_transcript(new_post['url']):
+                np = config.r.submission(id=new_post['name'])
+                np.reply(_(
                     yt_already_has_transcripts
                 ))
-                add_complete_post_id(new_post.fullname, config)
+                add_complete_post_id(new_post['name'], config)
                 logging.info(
-                    f'Found YouTube video, {get_yt_video_id(new_post.url)}, '
-                    f'with good transcripts.'
+                    f'Found YouTube video, {get_yt_video_id(new_post["url"])},'
+                    f' with good transcripts.'
                 )
                 return
         content_type = 'video'
@@ -107,7 +82,7 @@ def process_post(new_post, config):
 
     # Truncate a post title if it exceeds 250 characters, so the added
     # formatting still fits in Reddit's 300 char limit for post titles
-    post_title = new_post.title
+    post_title = new_post['title']
     max_title_length = 250
     if len(post_title) > max_title_length:
         post_title = post_title[:max_title_length - 3] + '...'
@@ -116,11 +91,11 @@ def process_post(new_post, config):
     try:
         result = config.tor.submit(
             title=discovered_submit_title.format(
-                sub=new_post.subreddit.display_name,
+                sub=new_post['subreddit'],
                 type=content_type.title(),
                 title=post_title
             ),
-            url=reddit_url.format(new_post.permalink)
+            url=reddit_url.format(new_post['permalink'])
         )
         result.reply(
             _(
@@ -133,22 +108,24 @@ def process_post(new_post, config):
         )
         flair_post(result, flair.unclaimed)
 
-        add_complete_post_id(new_post.fullname, config)
+        add_complete_post_id(new_post['name'], config)
         config.redis.incr('total_posted', amount=1)
 
         if config.OCR and content_type == 'image':
             # hook for OCR bot; in order to avoid race conditions, we add the
             # key / value pair that the bot isn't looking for before adding
             # to the set that it's monitoring.
-            config.redis.set(new_post.fullname, result.fullname)
-            config.redis.rpush('ocr_ids', new_post.fullname)
+            config.redis.set(new_post['name'], result.fullname)
+            config.redis.rpush('ocr_ids', new_post['name'])
 
         config.redis.incr('total_new', amount=1)
 
-    # I need to figure out what errors can happen here
+    # The only errors that happen here are on Reddit's side -- pretty much
+    # exclusively 503s and 403s that arbitrarily resolve themselves. A missed
+    # post or two is not the end of the world.
     except Exception as e:
         logging.error(
-            f'{e} - unable to post content.\nID: {new_post.fullname}\n '
-            f'Title: {new_post.title}\n Subreddit: '
-            f'{new_post.subreddit.display_name} '
+            f'{e} - unable to post content.\nID: {new_post["name"]}\n '
+            f'Title: {new_post["title"]}\n Subreddit: '
+            f'{new_post["subreddit"]}'
         )
