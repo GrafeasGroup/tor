be847eab92a895055b73c9c15f43db3d968613e6
David Alexander <opensource@thelonelyghost.com>
2019-02-08 19:03:58 -0500

    Fixes #150: constrains to the same major version of praw

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     install_requires=[
         'tor_core',
         'sh',
-        'praw',
+        'praw ~= 5.0.1',
     ],
     dependency_links=[
         'git+https://github.com/GrafeasGroup/tor_core.git@master#egg=tor_core-0',
