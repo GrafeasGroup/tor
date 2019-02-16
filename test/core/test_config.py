14dbc255d59e8f3dac80381fc6382545ee75c4b9
David Alexander <opensource@thelonelyghost.com>
2019-02-16 16:17:42 -0500

    Removes ModChat references in tests

--- test/core/test_config.py
+++ test/core/test_config.py
@@ -9,7 +9,6 @@ def test_read_secrets_from_filesystem():
     """Secret data has been read from the filesystem
     """
     assert SITE_CONFIG.bugsnag_api_key is not None
-    assert SITE_CONFIG.modchat_api_url is not None
 
 
 def test_config_structure():
@@ -40,9 +39,6 @@ def test_config_structure():
 
     assert isinstance(SITE_CONFIG.bugsnag_api_key, str) or \
         SITE_CONFIG.bugsnag_api_key is None
-
-    assert isinstance(SITE_CONFIG.modchat_api_url, str) or \
-        SITE_CONFIG.modchat_api_url is None
 
 
 def test_redis_config_property():
