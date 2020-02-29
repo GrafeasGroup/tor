d1a2320cda62837ddba9018483fe7b7d72d7a277
David Alexander <opensource@thelonelyghost.com>
2020-02-29 10:46:05 -0500

    Bumps v3.12.1 -> v3.12.2 due to Poetry issues

--- tor/__init__.py
+++ tor/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '3.12.1'
+__version__ = '3.12.2'
 __SELF_NAME__ = 'transcribersofreddit'
 __BOT_NAMES__ = os.environ.get('BOT_NAMES', 'transcribersofreddit,tor_archivist,transcribot').split(',')
 __root__ = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
