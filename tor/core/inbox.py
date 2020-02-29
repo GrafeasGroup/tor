d4d55cede84e6c9bb60db51d3fbc4cf5d295699d
David Alexander <opensource@thelonelyghost.com>
2020-02-29 15:09:44 -0500

    Merges tor.core.mentions into tor.core.inbox

--- tor/core/inbox.py
+++ tor/core/inbox.py
@@ -1,20 +1,19 @@
 import logging
 import re
 
-from praw.exceptions import ClientException as RedditClientException
-from praw.models import Comment as RedditComment
-from praw.models import Message as RedditMessage
+from praw.exceptions import ClientException
+from praw.models import Comment, Message
 from praw.models.reddit.mixins import InboxableMixin
 from tor.core import validation
 from tor.core.admin_commands import process_command, process_override
 from tor.core.config import Config
-from tor.core.helpers import send_to_modchat, is_our_subreddit
-from tor.core.mentions import process_mention
+from tor.core.helpers import send_to_modchat, is_our_subreddit, _
 from tor.core.strings import reddit_url
 from tor.core.user_interaction import (process_claim, process_coc,
                                        process_done, process_message,
                                        process_thanks, process_unclaim,
                                        process_wrong_post_location)
+from tor.strings import translation
 
 MOD_SUPPORT_PHRASES = [
     re.compile('fuck', re.IGNORECASE),
@@ -40,7 +39,7 @@ def forward_to_slack(item: InboxableMixin, cfg: Config) -> None:
     )
 
 
-def process_mod_intervention(post: RedditComment, cfg: Config) -> None:
+def process_mod_intervention(post: Comment, cfg: Config) -> None:
     """
     Triggers an alert in slack with a link to the comment if there is something
     offensive or in need of moderator intervention
@@ -70,7 +69,7 @@ def process_mod_intervention(post: RedditComment, cfg: Config) -> None:
     )
 
 
-def process_reply(reply: RedditComment, cfg: Config) -> None:
+def process_reply(reply: Comment, cfg: Config) -> None:
     try:
         r_body = reply.body.lower()  # cache that thing
 
@@ -107,7 +106,7 @@ def process_reply(reply: RedditComment, cfg: Config) -> None:
             # If we made it this far, it's something we can't process automatically
             forward_to_slack(reply, cfg)
 
-    except (RedditClientException, AttributeError) as e:
+    except (ClientException, AttributeError) as e:
         log.warning(e)
         log.warning(
             f"Unable to process comment {reply.submission.shortlink} "
@@ -116,6 +115,29 @@ def process_reply(reply: RedditComment, cfg: Config) -> None:
         # the only way we should hit this is if somebody comments and then
         # deletes their comment before the bot finished processing. It's
         # uncommon, but common enough that this is necessary.
+
+
+def process_mention(mention: Comment) -> None:
+    """
+    Handles username mentions and handles the formatting and posting of
+    those calls as workable jobs to ToR.
+
+    :param mention: the Comment object containing the username mention.
+    :return: None.
+    """
+    try:
+        i18n = translation()
+        pm_subject = i18n['responses']['direct_message']['subject']
+        pm_body = i18n['responses']['direct_message']['body']
+
+        # message format is subject, then body
+        mention.author.message(pm_subject, _(pm_body))
+        log.info(f'Message sent to {mention.author.name}!')
+    except (ClientException, AttributeError):
+        # apparently this crashes with an AttributeError if someone
+        # calls the bot and immediately deletes their comment. This
+        # should fix that.
+        pass
 
 
 def check_inbox(cfg: Config) -> None:
@@ -146,13 +168,13 @@ def check_inbox(cfg: Config) -> None:
         elif cfg.redis.sismember('blacklist', author_name):
             log.info(f'Skipping inbox item from {author_name!r} who is on the blacklist')
 
-        elif isinstance(item, RedditComment) and is_our_subreddit(item.subreddit.name, cfg):
+        elif isinstance(item, Comment) and is_our_subreddit(item.subreddit.name, cfg):
             process_reply(item, cfg)
-        elif isinstance(item, RedditComment):
+        elif isinstance(item, Comment):
             log.info(f'Received username mention! ID {item}')
             process_mention(item)
 
-        elif isinstance(item, RedditMessage):
+        elif isinstance(item, Message):
             if item.subject[0] == '!':
                 process_command(item, cfg)
             else:
