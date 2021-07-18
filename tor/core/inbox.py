abd6be0e231bede80cfc23ec4f230648dea8bc8a
Joe Kaufeld <joe@kenzie.academy>
2021-07-18 12:16:53 -0400

    blacken inbox.py

--- tor/core/inbox.py
+++ tor/core/inbox.py
@@ -8,14 +8,22 @@ from praw.models.reddit.mixins import InboxableMixin
 
 from tor import __BOT_NAMES__
 from tor.core import (
-    validation, CLAIM_PHRASES, DONE_PHRASES, MOD_SUPPORT_PHRASES, UNCLAIM_PHRASES
+    validation,
+    CLAIM_PHRASES,
+    DONE_PHRASES,
+    MOD_SUPPORT_PHRASES,
+    UNCLAIM_PHRASES,
 )
 from tor.core.admin_commands import process_command, process_override, process_debug
 from tor.core.config import Config
 from tor.core.helpers import _, is_our_subreddit, send_reddit_reply, send_to_modchat
 from tor.core.posts import get_blossom_submission
 from tor.core.user_interaction import (
-    process_claim, process_coc, process_done, process_message, process_unclaim
+    process_claim,
+    process_coc,
+    process_done,
+    process_message,
+    process_unclaim,
 )
 from tor.helpers.flair import flair_post
 from tor.strings import translation
@@ -25,23 +33,24 @@ i18n = translation()
 log = logging.getLogger(__name__)
 
 
-@beeline.traced(name='forward_to_slack')
+@beeline.traced(name="forward_to_slack")
 def forward_to_slack(item: InboxableMixin, cfg: Config) -> None:
     username = str(item.author.name)
 
     send_to_modchat(
         f'<{i18n["urls"]["reddit_url"].format(item.context)}|Unhandled message>'
-        f' by'
+        f" by"
         f' <{i18n["urls"]["reddit_url"].format("/u/" + username)}|u/{username}> -- '
-        f'*{item.subject}*:\n{item.body}', cfg
+        f"*{item.subject}*:\n{item.body}",
+        cfg,
     )
     log.info(
-        f'Received unhandled inbox message from {username}. \n Subject: '
-        f'{item.subject}\n\nBody: {item.body} '
+        f"Received unhandled inbox message from {username}. \n Subject: "
+        f"{item.subject}\n\nBody: {item.body} "
     )
 
 
-@beeline.traced(name='process_reply')
+@beeline.traced(name="process_reply")
 def process_reply(reply: Comment, cfg: Config) -> None:
     try:
         log.debug(f"Received reply from {reply.author.name}: {reply.body}")
@@ -49,19 +58,23 @@ def process_reply(reply: Comment, cfg: Config) -> None:
         flair = None
         r_body = reply.body.lower()  # cache that thing
 
-        if 'image transcription' in r_body or validation.contains_footer(reply, cfg):
-            message = _(i18n['responses']['general']['transcript_on_tor_post'])
-        elif matches := [match.group() for match in [regex.search(reply.body) for regex in MOD_SUPPORT_PHRASES] if match]:
+        if "image transcription" in r_body or validation.contains_footer(reply, cfg):
+            message = _(i18n["responses"]["general"]["transcript_on_tor_post"])
+        elif matches := [
+            match.group()
+            for match in [regex.search(reply.body) for regex in MOD_SUPPORT_PHRASES]
+            if match
+        ]:
             phrases = '"' + '", "'.join(matches) + '"'
             send_to_modchat(
                 ":rotating_light::rotating_light: Mod Intervention Needed "
                 ":rotating_light::rotating_light: "
-                f'\n\nDetected use of {phrases} {reply.submission.shortlink}',
-                cfg
+                f"\n\nDetected use of {phrases} {reply.submission.shortlink}",
+                cfg,
             )
-        elif 'thank' in r_body:  # trigger on "thanks" and "thank you"
-            thumbs_up_gifs = i18n['urls']['thumbs_up_gifs']
-            youre_welcome = i18n['responses']['general']['youre_welcome']
+        elif "thank" in r_body:  # trigger on "thanks" and "thank you"
+            thumbs_up_gifs = i18n["urls"]["thumbs_up_gifs"]
+            youre_welcome = i18n["responses"]["general"]["youre_welcome"]
             message = _(youre_welcome.format(random.choice(thumbs_up_gifs)))
         else:
             submission = reply.submission
@@ -71,7 +84,7 @@ def process_reply(reply: Comment, cfg: Config) -> None:
                 return
 
             blossom_submission = get_blossom_submission(submission, cfg)
-            if 'i accept' in r_body:
+            if "i accept" in r_body:
                 message, flair = process_coc(
                     username, reply.context, blossom_submission, cfg
                 )
@@ -82,13 +95,19 @@ def process_reply(reply: Comment, cfg: Config) -> None:
             elif r_body in CLAIM_PHRASES:
                 message, flair = process_claim(username, blossom_submission, cfg)
             elif r_body in DONE_PHRASES:
-                alt_text = 'done' not in r_body
+                alt_text = "done" not in r_body
                 message, flair = process_done(
-                    reply.author, blossom_submission, reply, cfg, alt_text_trigger=alt_text
+                    reply.author,
+                    blossom_submission,
+                    reply,
+                    cfg,
+                    alt_text_trigger=alt_text,
                 )
-            elif '!override' in r_body:
-                message, flair = process_override(reply.author, blossom_submission, reply.parent_id, cfg)
-            elif '!debug' in r_body:
+            elif "!override" in r_body:
+                message, flair = process_override(
+                    reply.author, blossom_submission, reply.parent_id, cfg
+                )
+            elif "!debug" in r_body:
                 message, flair = process_debug()
             else:
                 # If we made it this far, it's something we can't process automatically
@@ -109,7 +128,7 @@ def process_reply(reply: Comment, cfg: Config) -> None:
         )
 
 
-@beeline.traced(name='process_mention')
+@beeline.traced(name="process_mention")
 def process_mention(mention: Comment) -> None:
     """
     Handles username mentions and handles the formatting and posting of
@@ -119,12 +138,12 @@ def process_mention(mention: Comment) -> None:
     :return: None.
     """
     try:
-        pm_subject = i18n['responses']['direct_message']['subject']
-        pm_body = i18n['responses']['direct_message']['body']
+        pm_subject = i18n["responses"]["direct_message"]["subject"]
+        pm_body = i18n["responses"]["direct_message"]["body"]
 
         # message format is subject, then body
         mention.author.message(pm_subject, _(pm_body))
-        log.info(f'Message sent to {mention.author.name}!')
+        log.info(f"Message sent to {mention.author.name}!")
     except (ClientException, AttributeError):
         # apparently this crashes with an AttributeError if someone
         # calls the bot and immediately deletes their comment. This
@@ -132,7 +151,7 @@ def process_mention(mention: Comment) -> None:
         pass
 
 
-@beeline.traced(name='check_inbox')
+@beeline.traced(name="check_inbox")
 def check_inbox(cfg: Config) -> None:
     """
     Goes through all the unread messages in the inbox. It deliberately
@@ -151,7 +170,8 @@ def check_inbox(cfg: Config) -> None:
         if author_name is None:
             send_to_modchat(
                 f"We received a message without an author -- "
-                f"*{item.subject}*:\n{item.body}", cfg
+                f"*{item.subject}*:\n{item.body}",
+                cfg,
             )
         elif author_name == "transcribot":
             # bot responses shouldn't trigger workflows in other bots
