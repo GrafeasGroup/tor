6c4372ed8232ff78e49d7e1f2755ba0fa32a350f
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-07-24 14:51:57 +0200

    Add messages for each formatting error

--- tor/automatic_assessment/automatic_assessment.py
+++ tor/automatic_assessment/automatic_assessment.py
@@ -2,6 +2,10 @@ import re
 from typing import Optional, List, Set
 
 from tor.automatic_assessment.formatting_errors import FormattingError
+from tor.automatic_assessment.helpers import format_as_markdown_list
+from tor.strings import translation
+
+i18n = translation()
 
 FOOTER = "^^I'm&#32;a&#32;human&#32;volunteer&#32;content&#32;transcriber&#32;"
 "for&#32;Reddit&#32;and&#32;you&#32;could&#32;be&#32;too!&#32;[If&#32;you'd&#32;"
@@ -100,3 +104,10 @@ def check_transcription(transcription: str) -> Set[FormattingError]:
         ]
         if error is not None
     )
+
+
+def get_formatting_error_message(errors: Set[FormattingError]) -> str:
+    """Get a message containing instructions for each formatting error."""
+    error_messages = [i18n["automatic_assessment"][error.value] for error in errors]
+    error_list = format_as_markdown_list(error_messages)
+    return i18n["automatic_assessment"]["message"].format(error_list=error_list)
