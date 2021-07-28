a00a03a33ea623ddcef84554a86f9a4bf1402673
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-07-28 20:08:27 +0200

    Revert "Revert "Automatically detect common formatting issues in transcriptions (#238)""
    This reverts commit e4e107dd5dd6efbd8874800ff6a11a5e1c09632c.

---
+++ test/validation/test_formatting_validation.py
@@ -0,0 +1,187 @@
+from typing import List
+
+import pytest
+
+from tor.validation.formatting_validation import (
+    check_for_fenced_code_block,
+    check_for_missing_separators,
+    check_for_formatting_issues,
+    check_for_separator_heading,
+    check_for_malformed_footer,
+    check_for_bold_header,
+)
+from tor.validation.formatting_issues import FormattingIssue
+
+
+@pytest.mark.parametrize(
+    "test_input,should_match",
+    [
+        ("**Image Transcription: Tumblr**", True),
+        ("**Image Transcription:**", True),
+        ("**Image Transcription**", True),
+        ("**Video Transcription:**", True),
+        ("*Image Transcription: Tumblr*", False),
+        ("*Image Transcription:*", False),
+        ("*Image Transcription*", False),
+        (
+            "*Image Transcription: Tumblr*\n\n---\n\n**Image Transcription: Tumblr**",
+            False,
+        ),
+    ],
+)
+def test_check_for_bold_header(test_input: str, should_match: bool) -> None:
+    """Test if bold headers are detected."""
+    actual = check_for_bold_header(test_input)
+    expected = FormattingIssue.BOLD_HEADER if should_match else None
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,should_match",
+    [
+        ("This is a text", True),
+        ("This is a\n\n---\n\ntext", True),
+        ("This\n\n---\n\nis a\n\n---\n\ntext", False),
+        ("This\n\n---\n\nis\n\n---\n\na\n\n---\n\ntext", False),
+    ],
+)
+def test_check_for_missing_separators(test_input: str, should_match: bool) -> None:
+    """Test if missing separators are detected."""
+    actual = check_for_missing_separators(test_input)
+    expected = FormattingIssue.MISSING_SEPARATORS if should_match else None
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,should_match",
+    [
+        ("Heading\n---", True),
+        ("Heading with Spaces\n---", True),
+        ("Not Heading\n\n---", False),
+        ("Just text\nand\nstuff", False),
+    ],
+)
+def test_check_for_separator_headings(test_input: str, should_match: bool) -> None:
+    """Test if separators misused as headings are detected."""
+    actual = check_for_separator_heading(test_input)
+    expected = FormattingIssue.SEPARATOR_HEADINGS if should_match else None
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,should_match",
+    [
+        ("Text without footer", True),
+        (
+            "^(I'm a human volunteer content transcriber for Reddit and you could be too!) "
+            "[^(If you'd like more information on what we do and why we do it, click here!)]"
+            "(https://www.reddit.com/r/TranscribersOfReddit/wiki/index)",
+            True,
+        ),
+        (
+            "^^I'm&#32;a&#32;human&#32;volunteer&#32;content&#32;transcriber&#32;for&#32;and&#32;"
+            "you&#32;could&#32;be&#32;too!&#32;[If&#32;you'd&#32;like&#32;more&#32;information&#32;"
+            "on&#32;what&#32;we&#32;do&#32;and&#32;why&#32;we&#32;do&#32;it,&#32;click&#32;here!]"
+            "(https://www.reddit.com/r/TranscribersOfReddit/wiki/index)",
+            False,
+        ),
+    ],
+)
+def test_check_for_malformed_footer(test_input: str, should_match: bool) -> None:
+    """Test if malformed footers are detected."""
+    actual = check_for_malformed_footer(test_input)
+    expected = FormattingIssue.MALFORMED_FOOTER if should_match else None
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,should_match",
+    [
+        ("```\nCode\n```", True),
+        ("Word\n```\nint x = 1\n```Word", True),
+        ("This is just normal text", False),
+        ("Word\n    int x = 1\nWord", False),
+    ],
+)
+def test_check_for_fenced_code_block(test_input: str, should_match: bool) -> None:
+    """Test if fenced code blocks are detected."""
+    actual = check_for_fenced_code_block(test_input)
+    expected = FormattingIssue.FENCED_CODE_BLOCK if should_match else None
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        (
+            """*Image Transcription:*
+
+[*Description of Image.*]
+
+"""
+            "^^I'm&#32;a&#32;human&#32;volunteer&#32;content&#32;transcriber&#32;"
+            "for&#32;Reddit&#32;and&#32;you&#32;could&#32;be&#32;too!&#32;[If&#32;you'd&#32;"
+            "like&#32;more&#32;information&#32;on&#32;what&#32;we&#32;do&#32;and&#32;why&#32;"
+            "we&#32;do&#32;it,&#32;click&#32;here!](https://www.reddit.com/r/TranscribersOfReddit/wiki/index)",
+            [FormattingIssue.MISSING_SEPARATORS],
+        ),
+        (
+            """*Image Transcription:*
+
+---
+
+```
+function foo(x: int) {
+    return bar;
+}
+```
+
+---
+
+"""
+            "^^I'm&#32;a&#32;human&#32;volunteer&#32;content&#32;transcriber&#32;"
+            "for&#32;Reddit&#32;and&#32;you&#32;could&#32;be&#32;too!&#32;[If&#32;you'd&#32;"
+            "like&#32;more&#32;information&#32;on&#32;what&#32;we&#32;do&#32;and&#32;why&#32;"
+            "we&#32;do&#32;it,&#32;click&#32;here!](https://www.reddit.com/r/TranscribersOfReddit/wiki/index)",
+            [FormattingIssue.FENCED_CODE_BLOCK],
+        ),
+        (
+            """*Image Transcription:*
+
+```
+function foo(x: int) {
+    return bar;
+}
+```
+
+"""
+            "^^I'm&#32;a&#32;human&#32;volunteer&#32;content&#32;transcriber&#32;"
+            "for&#32;Reddit&#32;and&#32;you&#32;could&#32;be&#32;too!&#32;[If&#32;you'd&#32;"
+            "like&#32;more&#32;information&#32;on&#32;what&#32;we&#32;do&#32;and&#32;why&#32;"
+            "we&#32;do&#32;it,&#32;click&#32;here!](https://www.reddit.com/r/TranscribersOfReddit/wiki/index)",
+            [FormattingIssue.FENCED_CODE_BLOCK, FormattingIssue.MISSING_SEPARATORS],
+        ),
+        (
+            """*Image Transcription:*
+
+---
+
+[*Description of Image.*]
+
+---
+
+"""
+            "^^I'm&#32;a&#32;human&#32;volunteer&#32;content&#32;transcriber&#32;"
+            "for&#32;Reddit&#32;and&#32;you&#32;could&#32;be&#32;too!&#32;[If&#32;you'd&#32;"
+            "like&#32;more&#32;information&#32;on&#32;what&#32;we&#32;do&#32;and&#32;why&#32;"
+            "we&#32;do&#32;it,&#32;click&#32;here!](https://www.reddit.com/r/TranscribersOfReddit/wiki/index)",
+            [],
+        ),
+    ],
+)
+def test_check_for_formatting_issues(
+    test_input: str, expected: List[FormattingIssue]
+) -> None:
+    """Test if formatting issues are detected correctly"""
+    actual = check_for_formatting_issues(test_input)
+    assert actual == set(expected)
