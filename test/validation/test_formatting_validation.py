a57bb418694eaae43f1f9828b04b6e3b9ca1a5a0
Theodore Hua <theodorehuadev@gmail.com>
2021-08-08 10:05:01 -0400

    Add check for unescaped hashtags (#263)
    * Add check for unescaped hashtags
    
    * fix yaml spacing
    
    * make naming scheme more consistent, rename header to heading to avoid confusion
    
    * Improve checks to avoid false positivies
    
    * add more tests
    
    * Rename variables
    
    * fix naming inconsistency once more
    
    * refactor tests as well
    
    * change YAML message once more to heading
    
    * shorten regex
    
    * prevent tests from producing deprecation warning
    
    * change heading YAML
    
    * update

--- test/validation/test_formatting_validation.py
+++ test/validation/test_formatting_validation.py
@@ -14,6 +14,7 @@ from tor.validation.formatting_validation import (
     check_for_heading_with_dashes,
     check_for_malformed_footer,
     check_for_bold_header,
+    check_for_unescaped_heading,
     PROPER_SEPARATORS_PATTERN,
     HEADING_WITH_DASHES_PATTERN,
 )
@@ -173,6 +174,24 @@ def test_check_for_fenced_code_block(test_input: str, should_match: bool) -> Non
     """Test if fenced code blocks are detected."""
     actual = check_for_fenced_code_block(test_input)
     expected = FormattingIssue.FENCED_CODE_BLOCK if should_match else None
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,should_match",
+    [
+        ("#This is an unescaped heading", True),
+        (r"\#This is properly escaped", False),
+        ("# This is meant to render as a heading", False),
+        ("\n     \n   #heading", True),
+        ("\n\n\\#hashtag1 #hashtag2", False),
+        ("\n\n*#hashtag1 #hashtag2*", False),
+    ]
+)
+def test_check_for_unescaped_heading(test_input: str, should_match: bool) -> None:
+    """Test if unescaped hashtags are detected."""
+    actual = check_for_unescaped_heading(test_input)
+    expected = FormattingIssue.UNESCAPED_HEADING if should_match else None
     assert actual == expected
 
 
