51d4eeb1cb95b284a6a97101a37cc8758c263984
Joe Kaufeld <joe.kaufeld@gmail.com>
2021-10-25 05:49:46 -0400

    Add messages to update user on new rank (#282)
    * Done
    
    * Just added Initiate role
    
    * Update user_interaction.py
    
    * Fixed flair.py
    
    * Updated flair.py,user_interaction.py,en_US.yml
    
    * Updated flair.py and user_interaction.py
    
    * refactor to remove duplicated code
    
    * add separate names and simplify logic
    
    * Black will always win if flake8 and black is fighting. Ignore E231
    
    * add 'Initiate' rank
    
    Co-authored-by: Arnab1181412 <arnab.samanta.cd.met19@itbhu.ac.in>
    Co-authored-by: TimJentzsch <tim-jentzsch@gmx.de>

--- tor/helpers/flair.py
+++ tor/helpers/flair.py
@@ -1,4 +1,5 @@
 import logging
+import random
 
 from blossom_wrapper import BlossomStatus
 from praw.models import Comment, Redditor, Submission
@@ -6,8 +7,24 @@ from praw.models import Comment, Redditor, Submission
 from tor import __BOT_NAMES__
 from tor.core.config import Config
 from tor.core.helpers import clean_id, flair, send_to_modchat
+from tor.strings import translation
 
 log = logging.getLogger(__name__)
+i18n = translation()
+
+FLAIR_DATA = {
+    20000: {"class": "grafeas-sapphire", "name": "Sapphire"},
+    10000: {"class": "grafeas-jade", "name": "Jade"},
+    5000: {"class": "grafeas-topaz", "name": "Topaz"},
+    2500: {"class": "grafeas-ruby", "name": "Ruby"},
+    1000: {"class": "grafeas-diamond", "name": "Diamond"},
+    500: {"class": "grafeas-golden", "name": "Golden"},
+    250: {"class": "grafeas-purple", "name": "Purple"},
+    100: {"class": "grafeas-teal", "name": "Teal"},
+    50: {"class": "grafeas-green", "name": "Green"},
+    25: {"class": "grafeas-pink", "name": "Pink",},
+    1: {"class": "grafeas", "name": "Initiate"},
+}
 
 
 def flair_post(post: Submission, text: str) -> None:
@@ -37,28 +54,44 @@ def flair_post(post: Submission, text: str) -> None:
 
 
 def _get_flair_css(transcription_count: int) -> str:
-    if transcription_count >= 20000:
-        return 'grafeas-sapphire'
-    if transcription_count >= 10000:
-        return "grafeas-jade"
-    elif transcription_count >= 5000:
-        return "grafeas-topaz"
-    elif transcription_count >= 2500:
-        return "grafeas-ruby"
-    elif transcription_count >= 1000:
-        return "grafeas-diamond"
-    elif transcription_count >= 500:
-        return "grafeas-golden"
-    elif transcription_count >= 250:
-        return "grafeas-purple"
-    elif transcription_count >= 100:
-        return "grafeas-teal"
-    elif transcription_count >= 50:
-        return "grafeas-green"
-    elif transcription_count >= 25:
-        return "grafeas-pink"
-    else:
-        return "grafeas"
+    keys = list(FLAIR_DATA.keys())
+    keys.sort()  # arrange from smallest to largest
+    keys.reverse()  # rearrange from largest to smallest
+    # The only time we interact with this function should be with a positive
+    # value for the count, but a little extra validation never hurt.
+    if transcription_count < 1:
+        transcription_count = 1
+    return [FLAIR_DATA[i]["class"] for i in keys if i <= transcription_count][0]
+
+
+def check_promotion(count):
+    return True if count in FLAIR_DATA.keys() else False
+
+
+def generate_promotion_message(count: int) -> str:
+    keys = list(FLAIR_DATA.keys())
+    keys.sort()
+    text = i18n["responses"]["done"]["promotion_text"]
+    rank = [FLAIR_DATA[r].get("name") for r in keys if r == count][0]
+    exclamation = random.choice(text["exclamations"])
+
+    new_rank = (
+        text["new_rank"].format(rank=rank)
+        if rank and count != 1  # if the user is a newbie, show different message
+        else text["first_rank"]
+    )
+
+    try:
+        next_rank_obj = [FLAIR_DATA[r] for r in keys if r > count][0]
+        next_rank = text["next_rank"].format(
+            intro=random.choice(text["next_rank_intros"]),
+            rank=next_rank_obj["name"],
+            count=[k for k, v in FLAIR_DATA.items() if v == next_rank_obj][0],
+        )
+    except IndexError:
+        next_rank = text["highest_rank"]
+
+    return f"{exclamation} {new_rank} {next_rank}"
 
 
 def set_user_flair(user: Redditor, post: Comment, cfg: Config) -> None:
