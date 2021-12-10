e14f3d280561e752f80ceca08193f9f8526245bd
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-12-10 12:30:25 +0100

    Adjust formatting checks to allow new footer The new footer removes the 'for Reddit' part, to avoid misunderstandings who we work for.

--- test/validation/test_formatting_validation.py
+++ test/validation/test_formatting_validation.py
@@ -198,8 +198,18 @@ def test_check_for_heading_with_dashes(test_input: str, should_match: bool) -> N
             "(https://www.reddit.com/r/TranscribersOfReddit/wiki/index)",
             True,
         ),
+        # Old footer (with "for Reddit")
+        # TODO: We probably want to disallow this one at some point
         (
-            "^^I'm&#32;a&#32;human&#32;volunteer&#32;content&#32;transcriber&#32;for&#32;and&#32;"
+            "^^I'm&#32;a&#32;human&#32;volunteer&#32;content&#32;transcriber&#32;for&#32;Reddit&#32;and&#32;"
+            "you&#32;could&#32;be&#32;too!&#32;[If&#32;you'd&#32;like&#32;more&#32;information&#32;"
+            "on&#32;what&#32;we&#32;do&#32;and&#32;why&#32;we&#32;do&#32;it,&#32;click&#32;here!]"
+            "(https://www.reddit.com/r/TranscribersOfReddit/wiki/index)",
+            False,
+        ),
+        # New footer (without "for Reddit")
+        (
+            "^^I'm&#32;a&#32;human&#32;volunteer&#32;content&#32;transcriber&#32;and&#32;"
             "you&#32;could&#32;be&#32;too!&#32;[If&#32;you'd&#32;like&#32;more&#32;information&#32;"
             "on&#32;what&#32;we&#32;do&#32;and&#32;why&#32;we&#32;do&#32;it,&#32;click&#32;here!]"
             "(https://www.reddit.com/r/TranscribersOfReddit/wiki/index)",
