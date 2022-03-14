5a3ba8954163cbb445efe5b97ae816d0cf1e9177
Theodore Hua <theodorehuadev@gmail.com>
2022-03-14 09:05:49 -0400

    allow whitespace before header;resolve optional asterisk

--- tor/validation/formatting_validation.py
+++ tor/validation/formatting_validation.py
@@ -253,7 +253,7 @@ def check_for_invalid_header(transcription: str) -> Optional[FormattingIssue]:
         FormattingIssue.INVALID_HEADER
         if not any(
             [
-                re.search(r"^\**{}.*\*".format(i), header) is not None
+                re.search(r"^\s*\*+{}.*\*".format(i), header) is not None
                 for i in VALID_HEADERS
             ]
         )
